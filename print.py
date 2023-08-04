9c478a4d2631eb9fb42cd724a80ee5a1b7ca2000
Agnetah Zighe <agiezig11@gmail.com>
2023-08-04 19:21:30 +0300

    first commit

---
+++ print.py
@@ -0,0 +1,5 @@
+
+
+#this is basic information about me
+print("i am 21 years old")
+print("i am honoured to learn python")
---
+++ variables.py
@@ -0,0 +1,15 @@
+#understanding variables
+varx ="wembley"
+vary ="premier league"
+print(varx)
+print(vary)
+
+
+#about me
+valuebles="two things i love are black cats and music"
+motivation1="i want to learn programming inorder to be able to become a devops engineer.the second reason is i love showing off"
+motivation2="money is my biggest motivator in life.the second motivator is freedom,i want the freedom to do what i want"
+print(valuebles)
+print(motivation1)
+print(motivation2)
+
