689b177930cfe659cf8e368896657c45d054e802
Agnetah Zighe <agiezig11@gmail.com>
2023-08-12 16:42:08 +0300

    first branch commit

---
+++ comparison operator/code lab1.py
@@ -0,0 +1,35 @@
+x=30
+y=47
+print(x >= y)
+
+v="apple"
+b="goat"
+print(v <=b)
+
+#compare 7+5>=10
+a=7
+b=5
+print(7+5 >=10)
+
+#compare "anil">'anil'
+c="anil" > "anil"
+print(c)
+
+#compare 25%3==2
+d=25
+e=3
+q=2
+print(d%e==q)
+
+#compare 23//7>=9
+f=23
+g=7
+v=9
+print(f//g>v)
+
+h= False==False
+print= (h)
+
+j=True != False
+print=(j)
+
